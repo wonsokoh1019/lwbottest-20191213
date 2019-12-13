b3f7be1863a3870c933ecf7c1114f0372e749e5f
lwbottest_bgp <podgyjay@gmail.com>
2019-12-13 16:38:34 +0900

    test
    test

---
+++ conf/config.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+# default language ['en'|'ko'|'ja']
+DEFAULT_LANG = 'ja'
+
+# TimeZone
+TZone = "Asia/Tokyo"
+
+# account
+ADMIN_ACCOUNT = "gbptest@lwbottest"
+DOMAIN_ID = 153856
+
+# api
+API_ID = "kr1QrSGeTxomo"
+SERVER_CONSUMER_KEY = "m8KVmYHNWsE8NHoMITUT"
+
+SERVER_ID = "0132e857f9564c898fb9a87d1a92c441"
+SECRET_KEY_NAME = "private_20191212114227.key"
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https://lwbottest-gbp.herokuapp.com/"
+
+# DB config
+DB_HOST = "ec2-174-129-255-35.compute-1.amazonaws.com"
+DB_NAME = "da1nu2akov8p6s"
+DB_USER = "fqjdymoydqzwiz"
+DB_PORT = "5432"
+DB_PASSWORD = "526cca05c1d29fb8deab36fa079991d340644d159eff39bfc88bc258e79e98ed"
